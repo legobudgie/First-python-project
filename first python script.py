718afa53880495a4e547e394180c427984727a33
Bramwell Simpson <bramwellsimpson1@gmail.com>
2017-09-23 18:43:49 +0100

    I dunno

--- first python script.py
+++ first python script.py
@@ -1,24 +1,27 @@
 #If you're from the future and#
 #you're cringing, sorry. I was 14.#
 import time
-name = input("Enter your name: ")
-##if name.isalpha():
-##  print (name)
-##else:
-##      print ("Try again but with letters ;)")
-##      time.sleep(1)
-##      name = input("Enter your name punk: ")
+name = input("Enter your name punk: ")
+
 while (not name.isalpha()):
   print ("Try again but with letters ;)")
-  time.sleep(1)
-  name = input("Enter your name: ")
-  
-age = input("How old are you?: ")
-while (not age.isnumeric()):
-  print ("Try again but with numbers ;)")
-  time.sleep(1)
-  age = input("How old are you?: ") 
 
+  name = input("Enter your name punk: ")
+  
+age = input("How old are you, sucker?: ")
+##if age.isnumeric():
+##  print (age)
+##else:
+##   print ("Try again but with numbers ;)")
+##   time.sleep(1)
+##   age = input("How old are you, sucker?: ")
+
+while (not age.isnumeric()):
+    print ("Try again but with numbers ;)")
+
+    age = input("How old are you, sucker?: ")
+print (age)
+time.sleep(1)
 print ("OK, " + name + ", you are " + age + " years old?")
 time.sleep(1)
 verify = input("Is this correct? Y for yes, N for no(must be caps): ")
@@ -35,3 +38,9 @@ else:
     print("y tho")
     time.sleep(1)
     print("Sorry I couldn't loop at the time of making")
+
+    
+
+
+
+
